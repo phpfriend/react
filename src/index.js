1612d3bb96f770bba7c0c42184bdd2a871b59704
Yogesh Tiwari <yogesh.tiwari@intelligaia.com>
2020-11-11 16:46:30 +0530

    updated logic

--- src/index.js
+++ src/index.js
@@ -13,37 +13,25 @@ let morningtime = 'Morning';
 let eveTime = 'Evening';
 let starStr = 'Good';
 let myString = "Hello"+" "+ myName +" "+ sirName;
-let curTime =  new Date().toLocaleString('en-US', { hour: 'numeric', hour12: true });
-let res = curTime.split(" ");
+let curTime =  new Date().getHours(); 
+
 let specialString = '';
 let finalImage = '';
 
 
-switch(res[1]){
-  case 'AM':
-    if(res[0]<= 3){
-      specialString = starStr +" " + nightTime;
-      finalImage = nightImage;
-    } else if(res[0]>= 3 || res[0]<= 11){
-      specialString = starStr +" " + morningtime;
-      finalImage = mngImage;
-    }
-    break;
-  case 'PM': 
-  if(res[0]>= 12 || res[0]<= 4){
-    specialString = starStr +" " + noonTime;
-    finalImage = noonImage;
-  } else if(res[0]>= 5 || res[0]<= 8){
-    specialString = starStr +" " + eveTime;
-    finalImage = eveningImage;
-  } else if(res[0]>= 8){
-    specialString = starStr +" " + nightTime;
-    finalImage = nightImage;
-  }
-  break; 
-  default:
+if(curTime>=4 && curTime<=11){
+  specialString = starStr +" " + morningtime;
+  finalImage = mngImage;
+} else if(curTime>=12 && curTime<=16){
+  specialString = starStr +" " + noonTime;
+  finalImage = noonImage;
+} else if(curTime>=17 && curTime<=20){
+  specialString = starStr +" " + eveTime;
+  finalImage = eveningImage;
+} else if(curTime>=21 && curTime<=3){
+  specialString = starStr +" " + nightTime;
+  finalImage = nightImage;
 }
-
 ReactDOM.render(
   <>
   <div style={{
